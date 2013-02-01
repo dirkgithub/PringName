c7e1e1bf0e220e89f8d302d56cfa049ee24b255f
dirkgithub <dirk_github@github.com>
2013-02-01 20:07:18 +0800

    123

--- PrintName/src/com/iquanzhi/test/PrintYourName.java
+++ PrintName/src/com/iquanzhi/test/PrintYourName.java
@@ -4,5 +4,9 @@ public class PrintYourName {
 
 	public static void main(String[] args) {
 		System.out.println("my name is dirk.");
+		System.out.println("my name is dirk.");
+		System.out.println("my name is dirk.");
+		System.out.println("my name is dirk.");
+		System.out.println("my name is dirk.");
 	}
 }
